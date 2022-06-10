c50f2a43969d35228bd0bf0cd86d305f486fe17d
jdavids0 <jbrdavidson@gmail.com>
2022-06-10 11:59:05 -0600

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using DojoSurveyVal.Models;
+
+namespace DojoSurveyVal.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [HttpPost("process")]
+    public IActionResult FormSubmission(Survey survey)
+    {
+        return RedirectToAction ("Success", survey);
+    }
+
+    [HttpGet("success")]
+    public IActionResult Success(Survey survey)
+    {
+        // ViewBag.Survey = newSurvey;
+        return View("Success", survey);
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace DojoSurveyVal.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
